99e44b6bf93b8de5b5113210125f01d19377294e
XaXRaRXaX <robertjefferson2945@gmail.com>
2025-06-15 00:28:21 +0700

    Benerin login validasi dan tambahin error message di masing masing login field

--- Login.js
+++ Login.js
@@ -1,34 +1,41 @@
-const form = document.querySelector('.login-form');
+const form = document.getElementById('login-form');
 const email = document.getElementById('email-field')
 const password = document.getElementById('password-field')
 
 form.addEventListener('submit', (event) => {
-    let isEmailValid, emailErr = checkEmail(email.value.trim());
-    let isPasswordValid, passErr = checkPassword(password.value.trim());
+    let [isEmailValid, emailErr] = checkEmail(email.value.trim());
+    let [isPasswordValid, passErr] = checkPassword(password.value.trim());
 
     if(!isEmailValid || !isPasswordValid) {
         if(emailErr != null) {
-            // action
+            document.getElementById('email-warning').style.display = 'block';
+            document.getElementById('email-warning').innerHTML = emailErr;
         }
         if(passErr != null) {
-            // action
+            document.getElementById('password-warning').style.display = 'block';
+            document.getElementById('password-warning').innerHTML = passErr;
         }
+        
         event.preventDefault();
     }
+
+    document.getElementById('email-warning').style.display = 'none';
+    document.getElementById('password-warning').style.display = 'none';
+
 });
 
 const checkEmail = (value) => {
     const regx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(value === '') return false, "email should't be empty";
-    if(!regx.test(inputValue)) return false, "email format is invalid";
+    if(value === '') return [false, "email should't be empty"];
+    if(!regx.test(value)) return [false, "email format is invalid"];
 
-    return true;
+    return [true, null];
 };
 
 const checkPassword = (value) => {
     const pattern = /['"<>;]/
-    if(value < 8) return false, "must atleast 8 character";
-    if(pattern.test(value)) return false, "contain invalid characters";
+    if(value.length < 8) return [false, "must atleast 8 character"];
+    if(pattern.test(value)) return [false, "contain invalid characters"];
 
-    return true;
+    return [true, null];
 }
