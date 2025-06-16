6c57f9f2fee61130dcb6ea0eaf5c66a615b531b7
XaXRaRXaX <robertjefferson2945@gmail.com>
2025-06-16 23:00:54 +0700

    Benerin struktur folder lagi, tambahin database.go, landingpage controller dan benerin landingpage css masukin ke static

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"pawscan/config"
+	"pawscan/controllers/landingPageController"
+)
+
+func main() {
+	config.ConnectDB()
+	log.Println("Connection is established, continue to opening port")
+
+	// 1. Landing Page
+	http.HandleFunc("/", landingPageController.Welcome)
+
+
+	// handle css and js
+	http.Handle("/views/",
+	http.StripPrefix("/views/",
+		http.FileServer(http.Dir("views"))))
+	
+
+	log.Println("Server running on http://localhost:8080")
+	log.Println("Other url can be found on http://127.0.0.1:8080")
+	http.ListenAndServe(":8080", nil)
+}
